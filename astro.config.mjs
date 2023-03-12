c25d46224c8f041cb26670ad403b891139c3badd
FH-Development <felix.hartnagel@web.de>
2023-03-12 19:36:57 +0100

    Added Matomo Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import matomo from '@jop-software/astro-matomo';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,12 @@ export default defineConfig({
         tailwind(),
         image({
             serviceEntryPoint: "@astrojs/image/sharp",
+        }),
+        // We only track site views when this matches the host the user is on.
+        // If the configuration is empty, every page view gets tracked.
+        matomo({
+            baseUrl: "https://analytics.fhaf.de/",
+            siteId: 3
         }),
     ],
     vite: {
