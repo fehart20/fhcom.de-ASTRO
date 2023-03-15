85a21bb3f5421d41d8540ec62a1c6aa0ac5f83fc
FH-Development <felix.hartnagel@web.de>
2023-03-15 18:58:17 +0100

    Run Prettiert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import matomo from '@jop-software/astro-matomo';
+import matomo from "@jop-software/astro-matomo";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://fhcom.de/",
-    integrations: [
-        tailwind(),
-        image({
-            serviceEntryPoint: "@astrojs/image/sharp",
-        }),
-        // We only track site views when this matches the host the user is on.
-        // If the configuration is empty, every page view gets tracked.
-        matomo({
-            baseUrl: "https://analytics.fhaf.de/",
-            siteId: 3
-        }),
-    ],
-    vite: {
-        ssr: {
-            external: ["svgo"],
-        },
+  site: "https://fhcom.de/",
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    // We only track site views when this matches the host the user is on.
+    // If the configuration is empty, every page view gets tracked.
+    matomo({
+      baseUrl: "https://analytics.fhaf.de/",
+      siteId: 3,
+    }),
+  ],
+  vite: {
+    ssr: {
+      external: ["svgo"],
     },
+  },
 });
