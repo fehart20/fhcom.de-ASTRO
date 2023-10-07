4d89739fb8ae380e3dc217f711559bd96586814e
Felix Hartnagel <felix@fhcom.de>
2023-10-07 10:41:18 +0000

    Added astro-robots and impressum

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,32 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import matomo from "@jop-software/astro-matomo";
+import robots from "astro-robots";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fhcom.de/",
   integrations: [
     tailwind(),
+    robots({
+      sitemap: false,
+      policy: [
+        {
+          userAgent: [
+            "Applebot",
+            "Googlebot",
+            "bingbot",
+            "Yandex",
+            "Yeti",
+            "Baiduspider",
+            "360Spider",
+            "*",
+          ],
+          allow: ["/"],
+          disallow: ["/impressum", "/datenschutz"],
+        },
+      ],
+    }),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
